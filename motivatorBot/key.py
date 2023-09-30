d967b66c953438ea699a2731d59b6aaf81e7ff87
Aun Hassan <hassanaun520@gmail.com>
2023-09-30 17:32:14 +0500

    d

--- motivatorBot/key.py
+++ motivatorBot/key.py
@@ -1,3 +1,5 @@
 import os
 os.environ['Twilio_acc_sid'] = ("AC09115c383e9ce2d58e6e8c6a49152671")
 os.environ['Twlio_auth_token'] = ("210667de29ce5c10155fc0c67d08b51c")
+account_sid = os.environ.get("Twilio_acc_sid")
+auth_token = os.environ.get("Twlio_auth_token")
