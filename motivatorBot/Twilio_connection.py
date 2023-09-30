fcd9566ea43c37f37b7040b9e3755d10c5a09c79
AunHassan <108936791+AunHassan@users.noreply.github.com>
2023-09-30 17:23:14 +0500

    Update Twilio_connection.py

--- motivatorBot/Twilio_connection.py
+++ motivatorBot/Twilio_connection.py
@@ -1,7 +1,6 @@
 from twilio.rest import Client
 import os
-os.environ['Twilio_acc_sid'] = ("AC09115c383e9ce2d58e6e8c6a49152671")
-os.environ['Twlio_auth_token'] = ("210667de29ce5c10155fc0c67d08b51c")
+
 account_sid = os.environ.get("Twilio_acc_sid")
 auth_token = os.environ.get("Twlio_auth_token")
 client = Client(account_sid, auth_token)
